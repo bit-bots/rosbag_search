200113d5289abd3dd67bb213b942a0201c961d87
Lea <wedmann.lea@gmail.com>
2024-10-24 18:27:58 +0200

    olay around with tailwinds

--- rosbag_search/app.py
+++ rosbag_search/app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def rosbag_search():
+    return render_template('rosbag_search.html')
 
 @app.route("/<name>")
 def hello_lea(name=None):
